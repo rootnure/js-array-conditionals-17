e5f3bdd2a0a879dbe13fda7f18361b18c21d3dd5
rootnure <nur.diu.2791@gmail.com>
2023-08-01 17:45:48 +0600

    Array, array length, array index

---
+++ array.js
@@ -0,0 +1,6 @@
+var customerAges = [65, 41, 63, 49, 56, 84];
+var courseFees = [3200, 4000, 4900, 5000];
+var courseCodes = ['CSE421', 'CSE426', 'CSE427', 'CSE498'];
+var englishVowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(customerAges.length);
