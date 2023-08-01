82f78b5eea9f51e7677132f725895f9ffd0d3903
rootnure <nur.diu.2791@gmail.com>
2023-08-01 17:16:57 +0600

    variable recap

---
+++ variable.js
@@ -0,0 +1,32 @@
+// number type variable
+var sunglassPrice = 350;
+
+// string type variable
+var fullName = "Jennie Wennie";
+
+// boolean type variable
+var isPresent = false;
+
+// two types of number:
+// Integer: 120,
+// float, decimal: 3.69
+
+// variable naming convention: rules, best practice
+/**
+ * 1. single word
+ * 2. no gap or space
+ * 3. no quote
+ * 4. no keyword
+ * 5. can not start with a number
+ * 6. can not contain dash
+ * 7. should use camelCase
+ */
+
+// Math Operations: +, -, *, /
+// Shorthand: +=, -=, *=, /=, ++, --
+
+// convert: parseInt, parseFloat
+
+// toFixed
+
+// remainder: vagses --> %
