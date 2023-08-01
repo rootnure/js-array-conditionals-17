b87429266ae790c693e0adacf0245078e1c88abe
rootnure <nur.diu.2791@gmail.com>
2023-08-01 23:58:16 +0600

    comparion: <, >, ==, !=, <=, >=, &&, ||

---
+++ comparison.js
@@ -0,0 +1,26 @@
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 == 6);
+// console.log(6 == 6);
+// console.log(5 != 6);
+
+// less than or equal
+// console.log(5 <= 6);
+// console.log(6 <= 6);
+// console.log(16 <= 6);
+
+// grater than or equal
+// console.log(5 >= 6);
+
+var num1 = 56;
+var num2 = 89;
+console.log(num1 < num2);
+console.log(num1 > num2);
+console.log(num1 === num2);
+console.log(num1 !== num2);
+console.log(num1 <= num2);
+console.log(num1 >= num2);
+
+// multiple conditions (join using either && (logical AND) or || (logical OR))
+// money1 > money2 && result1 > result2 && height1 > height2
+// num1 > num2 || age1 > age2 || salary1 > salary2
