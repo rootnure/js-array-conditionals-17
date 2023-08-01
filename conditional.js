e70e9053d119aedf7792d4edad3f4f23b0ee0a84
rootnure <nur.diu.2791@gmail.com>
2023-08-02 00:28:10 +0600

    Conditional: if, if...else

---
+++ conditional.js
@@ -0,0 +1,35 @@
+var dressPrice = 3800;
+var myBudget = 5000;
+// If dress price is less than my budget, I will buy the dress
+/**
+ * If dress price is less than my budget
+ *      I will buy the dress
+ * 
+ * 
+ * if (condition) {
+ *      // will execute if the condition is true
+ * }
+ *      
+ */
+
+if(dressPrice < myBudget) {
+    // console.log('Notun dress pore biye bari jabo');
+}
+
+var chickenPrice = 500;
+var myMoney = 50;
+
+/**
+ * if chicken er price is less than my money
+ * i will eat chicken
+ * na hoi: else
+ * alu vorta khabo
+ */
+
+
+if(chickenPrice <= myMoney){
+    console.log('I will eat chicken!!!');
+}
+else{
+    console.log('I will eat smashed potato and lentil soup');
+}
