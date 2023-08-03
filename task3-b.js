ff04e9df7ebad2f3c539d0b62585d47e6427c552
rootnure <nur.diu.2791@gmail.com>
2023-08-03 20:36:55 +0600

    Practice Task 3-b solved

---
+++ task3-b.js
@@ -0,0 +1,11 @@
+// part 2
+var side1 = 9;
+var side2 = 8;
+var side3 = 9;
+
+if(side1 == side2 || side1 == side3 || side2 == side3){
+    console.log('The triangle is Isosceles');
+}
+else {
+    console.log('Triangle is not Isosceles');
+}
