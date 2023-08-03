f2881e7c25ae4973d7dda8cc6e171d42100c143a
rootnure <nur.diu.2791@gmail.com>
2023-08-03 19:48:13 +0600

    Summary

---
+++ summary.js
@@ -0,0 +1,32 @@
+// array declaration
+var destination = ['Sajek', 'Cox Bazar', 'Bali', 'Canada'];
+
+// get value by index
+console.log(destination[2]);
+
+// set/replace value by index
+destination[1] = 'Sundarban';
+
+// add new element to an array
+destination.push('Bangkok');
+
+// remove the last element of an array
+destination.pop(); // return the last element of an array
+var removedDestination = destination.pop(); // store the remove element in a variable
+
+// conditionals
+// >, <, ==, !=, >=, <=
+
+// multi stage condition
+if(destination[0] == 'Cox Bazar') {
+    
+}
+else if(destination[1]) {
+
+}
+else if(destination.length > 5){ // check the length of an array
+
+}
+else {
+    
+}
