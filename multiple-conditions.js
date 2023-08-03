d4599bc50c821a27d8756ff7d33b37cfdd850075
rootnure <nur.diu.2791@gmail.com>
2023-08-03 16:34:39 +0600

    Multiple Conditions: Conditions permutations-combinations

---
+++ multiple-conditions.js
@@ -0,0 +1,24 @@
+var isGraduated = true;
+var salary = 25000;
+var haveCar = false;
+
+/* if(isGraduated === true && salary > 50000) {
+    console.log("Let's get married");
+}
+else {
+    console.log('No honey, without money');
+} */
+
+/* if(isGraduated === true && salary > 50000 && haveCar === true) {
+    console.log("Let's get married");
+}
+else {
+    console.log('No honey, without money');
+} */
+
+if(isGraduated === true && salary > 50000 || haveCar === true) {
+    console.log("Let's get married");
+}
+else {
+    console.log('No honey, without money or car e');
+}
