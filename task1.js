c165227c386ae47edc0c282237c6c5415e78d55a
rootnure <nur.diu.2791@gmail.com>
2023-08-04 17:11:54 +0600

    Task1 updated with step-b

--- task1.js
+++ task1.js
@@ -1,10 +1,18 @@
 var fruits = ['Apple', 'Banana', 'Orange'];
 
 var indexOfBanana = fruits.indexOf('Banana');
+console.log(indexOfBanana);
 
-// console.log(positionOfBanana);
+console.log(fruits);
 
 var replaceFruitName = 'Watermelon';
 fruits[indexOfBanana] = replaceFruitName;
 
+console.log(fruits);
+// remove the last element from the array
+fruits.pop();
+console.log(fruits);
+
+// add new element in the last of an array
+fruits.push('Mango');
 console.log(fruits);
